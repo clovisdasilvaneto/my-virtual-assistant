664af2aebdce34b17d955254a4144bfe93bf3828
Clovis Neto <clovis.neto@liferay.com>
2017-04-28 19:59:39 -0300

    fix request bug

--- app.js
+++ app.js
@@ -76,3 +76,32 @@ function sendTextMessage({ sender, messageData }) {
 		}
 	})
 }
+
+
+
+
+
+
+
+
+
+request({
+	url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+	qs: {access_token:token},
+	method: 'POST',
+	json: {
+		setting_type:"call_to_actions",
+		thread_state:"new_thread",
+		call_to_actions:[
+			{
+				payload:"USER_DEFINED_PAYLOAD"
+			}
+		]
+	}
+}, function(error, response) {
+	if (error) {
+		console.log('Error sending messages: ', error)
+	} else if (response.body.error) {
+		console.log('Error: ', response.body.error)
+	}
+});
