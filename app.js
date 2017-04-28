882d669fce84a448488b91c493d52d43b74b89bc
Clovis Neto <clovis.neto@liferay.com>
2017-04-28 19:30:12 -0300

    fix request bug

--- app.js
+++ app.js
@@ -28,44 +28,7 @@ app.get('/webhook/', (req, res)=>{
 	res.send('Error, wrong token')
 });
 
-app.post('/webhook/' (req, res)=>{
-	console.log(req)
-})
-
-// Spin up the server
-app.listen(app.get('port'),()=>{
-	console.log('running on port', app.get('port'))
-});'use strict';
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const config = require('./config/config.js');
-const app = express();
-const token = process.env.FB_PAGE_ACCESS_TOKEN;
-
-app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json());
-
-// Index route
-app.get('/', (req, res)=>{
-	res.send('Hello world, I am a chat bot')
-});
-
-// for Facebook verification
-app.get('/webhook/', (req, res)=>{
-	if (req.query['hub.verify_token'] === 'my_bot_verify') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-});
-
-app.post('/webhook/' (req, res)=>{
+app.post('/webhook/', (req, res)=>{
 	console.log(req)
 })
 
