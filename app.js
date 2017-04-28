14e10a9c8b2ecb2ebc5e2d3e4c2bf57d2358867a
Clovis Neto <clovis.neto@liferay.com>
2017-04-28 20:43:34 -0300

    fix request bug

--- app.js
+++ app.js
@@ -115,18 +115,6 @@ function setupBotLayout(){
 					type:"postback",
 					title:"Visualizar contas deste mês",
 					payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_START_ORDER"
-				},
-				{
-					type:"web_url",
-					title:"Site do autor",
-					url:"https://clovisdasilvaneto.github.io",
-					webview_height_ratio: "full",
-					messenger_extensions: true
-				},
-				{
-					type:"web_url",
-					title:"Facebook do autor",
-					url:"https://www.facebook.com/ClovisDaSilvaNeto"
 				}
 			]
 		}
