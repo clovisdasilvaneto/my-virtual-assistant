1617cc0ec614249105b7129aea2789e76adddfe1
Clovis Neto <clovis.neto@liferay.com>
2017-04-28 20:06:12 -0300

    fix request bug

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.listen(app.get('port'),()=>{
 function formatEntry(entry){
 	let pageID = entry.id;
 	let timerOfEvent = entry.time;
-	
+	console.log(data);
 	if(entry.message){
 		entry.message.forEach(formatEntryMessage);
 	}
@@ -79,31 +79,25 @@ function sendTextMessage({ sender, messageData }) {
 	})
 }
 
-
-
-
-
-
-
-
-
-request({
-	url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-	qs: {access_token:token},
-	method: 'POST',
-	json: {
-		setting_type:"call_to_actions",
-		thread_state:"new_thread",
-		call_to_actions:[
-			{
-				payload:"USER_DEFINED_PAYLOAD"
-			}
-		]
-	}
-}, function(error, response) {
-	if (error) {
-		console.log('Error sending messages: ', error)
-	} else if (response.body.error) {
-		console.log('Error: ', response.body.error)
-	}
-});
+function setupBotLayout(){
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			setting_type:"call_to_actions",
+			thread_state:"new_thread",
+			call_to_actions:[
+				{
+					payload:"USER_DEFINED_PAYLOAD"
+				}
+			]
+		}
+	}, function(error, response) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	});
+}
