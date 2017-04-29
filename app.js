f354db48359dc866ae1d1e60b234de5cda7a7b7a
Clovis Neto <clovis.neto@liferay.com>
2017-04-29 11:54:05 -0300

    fix request bug

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ function checkMessageToReply({message, sender}){
 	switch (message.text) {
 		case "USER_DEFINED_PAYLOAD":
 			sendMessage(sender, {
-				text: 'Olá amigo, eu sou o seu mais novo Assistente Virtual e estou aqui para lhe auxiliar nos pagamentos da sua conta! <br> Informe novas contas teclando: "Nova conta" e eu lhe pedirei mais informações. <br> Uma semana antes do vencimento da sua conta ficarei lhe lembrando de pagar a mesma.'
+				text: 'Olá amigo, eu sou o seu mais novo Assistente Virtual e estou aqui para lhe auxiliar nos pagamentos da sua conta! <br> Informe novas contas teclando: "Nova conta", em seguida pedirei mais informações e uma semana antes do vencimento da sua conta ficarei lhe lembrando de pagar a mesma.'
 			});
 			break
 		
